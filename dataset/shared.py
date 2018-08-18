acc35770c23a24930624cecf2fa0a34785373dd1
lizhimins <707364882@qq.com>
2018-08-18 23:12:25 +0800

    Path setting

---
+++ dataset/shared.py
@@ -0,0 +1,23 @@
+import itertools
+from os import makedirs
+import os
+from os.path import expanduser, join
+
+# Default folders
+dir_root = join(expanduser('~'), 'imagenet')
+# dir_root = os.getcwd()
+# print(dir_root)
+dir_originals = join(dir_root, 'original')
+dir_resized = join(dir_root, 'resized')
+dir_tfrecord = join(dir_root, 'tfrecords')
+dir_metrics = join(dir_root, 'metrics')
+dir_checkpoints = join(dir_root, 'checkpoints')
+
+
+def maybe_create_folder(folder):
+    makedirs(folder, exist_ok=True)
+
+
+def progressive_filename_generator(pattern='file_{}.ext'):
+    for i in itertools.count():
+        yield pattern.format(i)
