bd657964caaecff07e0d16503d3e482b676f7bc9
lizhimins <707364882@qq.com>
2019-01-12 14:32:36 +0800

    Update rename.py

--- dataset/rename.py
+++ dataset/rename.py
@@ -6,6 +6,9 @@ path = '/root/imagenet/resized'
 count = 1
 
 for file in os.listdir(path):
-    print(count,path,file)
-    os.rename(os.path.join(path,file),os.path.join(path,str(count)+".jpeg"))
+    # print(count,path,file)
+    #if (int(file[-12:-5]) > 99990):
+    #    print(file, str(int(file[-12:-5])))
+    print(os.path.join(path,file),os.path.join(path,str(int(file[-12:-5]))+".jpeg"))
+    os.rename(os.path.join(path,file),os.path.join(path,str(int(file[-12:-5]))+".jpeg"))
     count += 1
